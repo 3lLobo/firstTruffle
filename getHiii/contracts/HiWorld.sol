2849166b1624ccc88a2869f6f5dcdf9f6b95a713
3lLobo <flocwolf@gmail.com>
2021-12-09 17:44:10 +0100

    Real contracts!

--- getHiii/contracts/HiWorld.sol
+++ getHiii/contracts/HiWorld.sol
@@ -2,6 +2,15 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HiWorld {
+
+    function checkOwner(string calldata caller) private view returns (bool) {
+        require(msg.sender == caller);
+        revert();
+        assert(msg.sender == caller);
+    }
+
+    
+
     string private hiii = "Hi strijder!";
 
     function getHiii() public view returns (string memory) {

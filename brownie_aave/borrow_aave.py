51da6147a5f79f22a148352f920c9fee68a0b917
3lLobo <flocwolf@gmail.com>
2022-01-29 02:18:40 +0100

    killed the streak :skull:

--- brownie_aave/borrow_aave.py
+++ brownie_aave/borrow_aave.py
@@ -1,9 +1,16 @@
 """Borrow Aave coin"""
+from brownie import network, config
+from scripts.utils import get_account
 
 
 def borrow_aave():
     """Borrow aave
     """
+    pass
+
+def approve_erc20():
+    """Approve the transfer of
+    a ERC20 token."""
     pass
 
 def main():
--- brownie_aave/scripts/get_weth.py
+++ brownie_aave/scripts/get_weth.py
@@ -1,23 +1,25 @@
 """Mint wether by borrowing eth.
 """
 
-
-from idna import valid_contextj
 from scripts.utils import get_account
 from brownie import interface, network, config
 from web3 import Web3
 
+
 def get_weth():
-    """get weth function
-    """
+    """get weth function"""
     acc = get_account()
-    print("ETH balance: ", Web3.fromWei(acc.balance(), 'ether'))
-    weth = interface.WethInterface(config['networks'][network.show_active()]['weth_token'])
-    txx = weth.deposit({'from': acc, 'value': acc.balance()/2})
-    print('Recieved weth')
+    print(acc)
+    print("ETH balance: ", Web3.fromWei(acc.balance(), "ether"))
+    weth = interface.WethInterface(
+        config["networks"][network.show_active()]["weth_token"]
+    )
+    txx = weth.deposit({"from": acc, "value": 1 * 10 ** 18})
+    txx.wait(1)
+    print("Recieved weth!")
+    print("WETH balance: ", weth.balanceOf(acc))
+    print("ETH balance: ", Web3.fromWei(acc.balance(), "ether"))
     return txx
-
-
 
 
 def main():
